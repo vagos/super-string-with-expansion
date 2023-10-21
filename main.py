48c38550f79c257ec82014b283b02653faa58fa5
Evangelos Lamprou <e.lamprou@upnet.gr>
2023-10-22 00:36:38 +0200

    Immediately reject repeated letter case
    Trying this out. There might exist edge case I'm not accounting for.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import itertools
 import math
 import random
@@ -64,7 +65,6 @@ def read_data():
 def is_expansion_valid(string, strings, expansion):
     def expand_string(string, expansion):
         for k in expansion:
-            if k not in string: pass
             string = string.replace(k, expansion[k])
         return string
     for s in strings:
@@ -80,17 +80,48 @@ def remove_invalid_choices(string, expansions, strings):
         letters = letters.union(set(s))
     for k in set(expansions.keys()):
         if k not in letters:
+            # print(f"Removing {k}", file=sys.stderr)
             del expansions[k]
     # If a possible expansion in the expansions is not a substring of string, remove it 
     for l in expansions:
         for s in set(expansions[l]):
             if s not in string:
+                # print(f"Removing {s} from {l}", file=sys.stderr)
                 expansions[l].remove(s)
+
+# Handle the case where one of t_i strings is of the form AAA and there is no XXX in the target string
+def can_handle_repeated_letters(string, expansions, strings):
+    def get_repeated_letter_count(string):
+        c = ''
+        count = 0
+        max_count = 0
+        for i in range(len(string)):
+            if string[i] == c:
+                count += 1
+                max_count = max(max_count, count)
+            else:
+                c = string[i]
+                count = 1
+        return max_count
+
+    n_repeated_letters_string = get_repeated_letter_count(string)
+    # print(f"n_repeated_letters_string = {n_repeated_letters_string}", file=sys.stderr)
+
+
+    for s in strings:
+        n_repeated_letters_s = get_repeated_letter_count(s)
+        if n_repeated_letters_s == 0: continue
+        # print(f"s = {s}, n_repeated_letters_s = {n_repeated_letters_s}", file=sys.stderr)
+        if n_repeated_letters_s > n_repeated_letters_string:
+            return False
+    return True
+        
 
 def run_brute_force(string, expansions, strings):
     keys, values = zip(*expansions.items())
     for v in itertools.product(*values):
         expansion = dict(zip(keys, v))
+        # print(f"Trying {expansion}", file=sys.stderr)
         
         if is_expansion_valid(string, strings, expansion):
             return expansion
@@ -103,23 +134,29 @@ def print_answer(expansion, expansions):
     for k,v in expansion.items():
         print(f"{k}:{v}")
 
+def print_no():
+    print("NO")
+    exit(0)
+
 def main():
     random.seed(42)
-    expansions = None
-    string = None
-    strings = None
-    expansions_cpy = None
+    expansions = {}
+    string = ""
+    strings = set()
+    expansions_cpy = {}
 
     try:
         string, expansions, strings = read_data()
         expansions_cpy = dict(expansions)
     except:
-        answer = None
+        print_no()
 
-    try:
-        remove_invalid_choices(string, expansions, strings)
+    if not can_handle_repeated_letters(string, expansions, strings):
+        print_no()
+    remove_invalid_choices(string, expansions, strings)
+    if expansions:
         answer =  run_brute_force(string, expansions, strings)
-    except:
+    else:
         answer = None
     
     if expansions == {}:
@@ -128,7 +165,7 @@ def main():
     if answer is not None:
         print_answer(answer, expansions_cpy)
     else:
-        print("NO")
+        print_no()
 
 if __name__ == '__main__':
     main()
