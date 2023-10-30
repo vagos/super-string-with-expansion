6264d545599a1b0f2b9c08041b450b3d75ea5a17
Evangelos Lamprou <e.lamprou@upnet.gr>
2023-10-30 16:58:04 +0100

    Remove unecessary code, clarity renaming

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import sys
-import time
 import itertools
-import math
-import re
-from collections import defaultdict
 
 # Getting data from stdin
 class Parser:
@@ -62,7 +58,7 @@ def read_data():
     return string, expansions, strings
 
 def remove_invalid_choices(string, expansions, strings):
-    # Remove expansion of keys that do not appear into the target string (s)
+    # Remove R_j of letters that do not appear into any of the strings t_i
     letters = set()
     for s in strings:
         letters = letters.union(set(s))
@@ -76,10 +72,6 @@ def remove_invalid_choices(string, expansions, strings):
             if s not in string:
                 expansions[l].remove(s)
     
-    for e in expansions:
-        if len(expansions[e]) == 0:
-            del expansions[e]
-
     # If one of strings contains a letter to which no expansion is assigned, problem is unsolvable
     letters = set()
     for s in strings:
@@ -128,27 +120,27 @@ def main():
     expansions = {} # Dictionary that maps letters in Gamma to subset R_j
     string = ""     # Target string s
     strings = set() # Set of strings t_i
-    expansions_cpy = {}
+    expansions_pruned = {}
 
     try:
         string, expansions, strings = read_data()
     except:
         print_no()
 
-    expansions_cpy = {key: value[:] for key, value in expansions.items()}
+    expansions_pruned = {key: value[:] for key, value in expansions.items()}
 
-    solvable = remove_invalid_choices(string, expansions, strings)
+    solvable = remove_invalid_choices(string, expansions_pruned, strings)
 
-    if expansions and solvable:
-        answer =  run_brute_force(string, expansions, strings)
+    if expansions_pruned and solvable:
+        answer =  run_brute_force(string, expansions_pruned, strings)
     else:
         answer = None
     
-    if expansions == {}:
+    if expansions_pruned == {}:
         answer = {}
 
     if answer is not None:
-        print_answer(answer, expansions_cpy)
+        print_answer(answer, expansions)
     else:
         print_no()
 
